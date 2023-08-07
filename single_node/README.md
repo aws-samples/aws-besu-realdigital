d2a6f175ffddf9d8654ad2914f51e31780d70c4c
Amanda Quinto <amandaqt@amazon.com>
2023-08-07 23:12:56 +0000

    feat: adding single node files

--- single_node/README.md
+++ single_node/README.md
@@ -4,7 +4,7 @@
 This repo intends to support the creation of a single node Besu Hyperledger running into Amazon EC2 in private Amazon VPC using AWS CDK.
 
 
-## How to 
+## How to Install 
 
 ### 1. Configure Python and download CDK. 
 ```
@@ -30,13 +30,32 @@ Once the virtualenv is activated, you can install the required dependencies.
 $ pip install -r requirements.txt
 ```
 
+## 1.1 Parameters
+We are using parameters to define Network CIDR blocks, Instance Type and Bootnodes address. 
+
+### VPC CIDR
+The network address for the VPC that you will create on the stack.
+
+### Besu CIDR
+The network address of the Cluster Besu.
+
+### Instance Type
+The instance type of the stack. Default is m5.xlarge.
+
+### Bootnodes 
+To understand what bootnodes means and to get the BACEN address, check [this link](https://github.com/bacen/pilotord-kit-onboarding/blob/main/ingresso.md#discovery-bootnode).
+
+## 2. Synthesize the CDK 
+
 At this point you can now synthesize the CloudFormation template for this code.
 
 ```
-$ cdk synth
+$ cdk synth -c "VpcCIDR=10.0.0.0/16" -c "BesuCIDR=192.168.10.10/32" -c "InstanceType=c5.large" -c "BootNodes=enode://c35c3ec90a8a51fd5703594c6303382f3ae6b2ecb99bab2c04b3794f2bc3fc2631dabb0c08af795787a6c004d8f532230ae6e9925cbbefb0b28b79295d615f@127.0.0.1:30303"
 ```
 
-###  CDK Helpfull commands
+*this address and bootnodes are just examples*
+
+### 2.1  CDK Helpfull commands
 
  * `cdk ls`          list all stacks in the app
  * `cdk synth`       emits the synthesized CloudFormation template
@@ -44,17 +63,28 @@ $ cdk synth
  * `cdk diff`        compare deployed stack with current state
  * `cdk docs`        open CDK documentation
 
-### 2. Get the Besu configuration files 
 
-TBD 
+## 3. Deploy 
 
-### 3. Restart Besu with the new files
+To deploy the stack, run: 
 
-To start properly, you need to configure two files: `config.toml` and `genesis.json`. 
+`cdk deploy -c "VpcCIDR=10.0.0.0/16" -c "BesuCIDR=192.168.10.10/32" -c "InstanceType=c5.large" -c "BootNodes=enode://YourBootNode@127.0.0.1:30303"` 
 
-[BACEN files:](https://github.com/bacen/pilotord-kit-onboarding/blob/main/ingresso.md) 
+*Dont forget to check the [Parameters](#parameters) section*
 
-### 4. Check if Besu is Working
+## 4. EC2 SSH Connection
+
+To access your ECS instance, use [EC2 Session Manager](https://repost.aws/knowledge-center/ec2-systems-manager-vpc-endpoints).
+
+
+## 5. Besu Configuration
+
+The hyperledger Besu uses 2 configuration files: config.toml and genesis.json. In the CDK we are using the Bacen files. You can check it [here](https://github.com/bacen/pilotord-kit-onboarding/blob/main/ingresso.md#configura%C3%A7%C3%A3o-do-n%C3%B3-do-participante)
+
+*Note: If you are not using the Besu Hyperledger to connect into BACEN, you can change that in the file:/aws-besu-realdigital/single_node/real_digital_besu_ec2/real_digital_besu_ec2_stack.py and session: ## DOWNLOAD BACEN CONFIG FILES ##.*
+
+
+### 5.1 Check if Besu is Working
 
 To check if the Besu is connected to the chain: 
 
@@ -63,5 +93,17 @@ To check if the Besu is connected to the chain:
 To check the block sync: 
 
 `curl -X POST --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' localhost:8545`
+
+### 5.2 Troubleshooting
+
+If you are getting the error: 
+`bash[2901]: Unable to load genesis file. java.io.FileNotFoundException: /caminho/para/o/arquivo/genesis.json (No such file or directory)` 
+
+Its because you should edit the config.toml downloaded from Bacen, and change the lines
+
+data-path="/caminho/para/a/pasta/data"
+genesis-file="/caminho/para/o/arquivo/genesis.json"
+
+to the correctly path. 
 
 Enjoy!
--- single_node/real_digital_besu_ec2/real_digital_besu_ec2_stack.py
+++ single_node/real_digital_besu_ec2/real_digital_besu_ec2_stack.py
@@ -1,6 +1,6 @@
 from constructs import Construct
 from aws_cdk import (
-    Stack,
+    Stack, CfnParameter,
     aws_ec2 as ec2,
     aws_iam as iam
 )
@@ -9,10 +9,19 @@ class RealDigitalBesuEc2Stack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        
+        # vpc_cidr = CfnParameter(self, "vpcCdr", type="String",
+        #     description="The VPC CIDR address.")
+
+        # besu_cluster_cidr = CfnParameter(self, "besuCdr", type="String",
+        #     description="The CIDR of the cluster Besu where you will connect")
+
+        # instance_type = CfnParameter(self, "instanceType", type="String",
+        #     description="Instance Type. Default: m5.xlarge",
+        #     default="m5.xlarge"
+        # )
         # VPC # 
         vpc = ec2.Vpc(self, "Vpc",
-            ip_addresses=ec2.IpAddresses.cidr("192.168.0.0/16")
+            ip_addresses=ec2.IpAddresses.cidr(self.node.try_get_context('VpcCIDR'))
         )
         
         # IAM Role # 
@@ -31,15 +40,22 @@ class RealDigitalBesuEc2Stack(Stack):
         sec_grp= ec2.SecurityGroup(self, 'ec2-sec-grp', vpc=vpc, allow_all_outbound=True)
 
         sec_grp.add_ingress_rule(
-            peer=ec2.Peer.ipv4('172.16.0.0/24'), 
-            description='inbound SSH', 
-            connection=ec2.Port.tcp(22))
+            peer=ec2.Peer.ipv4(self.node.try_get_context('BesuCIDR')), 
+            description='Besu cluster',
+            connection=ec2.Port.tcp_range(30000, 30009)
+        )
 
+        sec_grp.add_ingress_rule(
+            peer=ec2.Peer.ipv4(self.node.try_get_context('BesuCIDR')), 
+            description='Besu Cluster', 
+            connection=ec2.Port.tcp(30303)
+        )
+        
         # EC2 Instance #
         instance = ec2.Instance(
             self
             , "Instance"
-            , instance_type=ec2.InstanceType("t2.micro")
+            , instance_type=ec2.InstanceType(self.node.try_get_context('InstanceType'))
             , machine_image=ec2.MachineImage.latest_amazon_linux(
                 generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
             )
@@ -47,8 +63,11 @@ class RealDigitalBesuEc2Stack(Stack):
             , role=role
             , security_group =sec_grp
         )
+        
+        BootNodeAddress = self.node.try_get_context('BootNodes')
         commands = '''
 #!/bin/bash
+
 echo " ##  INIT USER DATA ## "
 sudo yum update -y 
 
@@ -71,13 +90,19 @@ touch /etc/genesis/genesis.json
 
 wget https://hyperledger.jfrog.io/hyperledger/besu-binaries/besu/23.4.1/besu-23.4.1.tar.gz / sha256: 49d3a7a069cae307497093d834f873ce7804a46dd59207d5e8321459532d318e
 
- tar -xvzf besu-23.4.1.tar.gz
+tar -xvzf besu-23.4.1.tar.gz
 
 cd besu-23.4.1/
 
 bin/besu --help
 
+echo "## DOWNLOAD CONFIG FILES"
+cd /usr/lib/besu/besu-23.4.1
+wget https://raw.githubusercontent.com/bacen/pilotord-kit-onboarding/main/genesis.json
+wget https://raw.githubusercontent.com/bacen/pilotord-kit-onboarding/main/config.toml
+
 echo "## CONFIG BESU ON INIT ##"
+
 sudo touch /usr/lib/systemd/system/besu.service
 
 sudo cat <<EOF >/usr/lib/systemd/system/besu.service
@@ -88,20 +113,23 @@ Description=Besu Enterprise Ethereum java client
 User=root
 Type=oneshot
 WorkingDirectory=/usr/lib/besu/besu-23.4.1
-ExecStart= /bin/bash -c "cd /usr/lib/besu/besu-23.4.1 && bin/besu --config-file=config.toml --min-gas-price=0 --bootnodes=enode:x-x-x-x//@0.0.0.0:30303,enode:x-x-x-x//@0.0.0.0:30303"
+ExecStart= /bin/bash -c "cd /usr/lib/besu/besu-23.4.1 && bin/besu --config-file=config.toml --min-gas-price=0 --bootnodes=enode$BOOTNODE_ADDRESS"
 SuccessExitStatus=143
-Restart=on-failure
-RestartSec=10s
+
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
+
 echo "## START BESU ##"
-# Create and start service besu # 
+echo "# Create and start service besu #"
+
 systemctl enable besu
 systemctl daemon-reload
 systemctl start besu
+systemctl status besu
 '''
-
+        commands = commands.replace("BOOTNODE_ADDRESS", BootNodeAddress)
         instance.user_data.add_commands(commands)
 
