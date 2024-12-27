9086b1b6b06241d5cff14286ab7ebd2ee40bf562
Gustavo Carreira <gucarr@amazon.com>
2024-12-27 19:43:29 -0300

    bump providers version

--- cluster-besu/versions.tf
+++ cluster-besu/versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = ">= 5.34"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
+      version = ">= 2.23"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.9"
+      version = ">= 2.11"
     }
     time = {
       source  = "hashicorp/time"
@@ -20,11 +20,16 @@ terraform {
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
+      version = ">= 1.18"
     }
     null = {
       source  = "hashicorp/null"
       version = "3.1.0"
     }
+  }
+  backend "s3" {
+    bucket = "xxxxxxxxxxxxx"
+    key    = "cluster-besu/tfstate/cluster-besu.tfstate"
+    region = "us-east-1"
   }
 }
